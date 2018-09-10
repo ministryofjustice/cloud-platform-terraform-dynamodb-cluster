bf04129971ec980a8a9769a5ac2e815e0e8600c1
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2018-09-10 11:23:11 +0100

    tf version

--- example/main.tf
+++ example/main.tf
@@ -1,11 +1,10 @@
 provider "aws" {
   region  = "eu-west-1"
-  version = "~> 1.17.0"
+  version = ">= 1.17.0"
 }
 
 module "example_team_dynamodb" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-dynamodb-cluster?ref=v1.0"
-  # source = "../"
 
   team_name              = "example-team"
   business-unit          = "example-bu"
