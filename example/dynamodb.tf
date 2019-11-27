ba4b96204e5bd9b6330587e3a653232c5bbf02f2
vijay-veeranki-moj <vijay.veeranki@digital.justice.gov.uk>
2019-11-27 14:33:59 +0000

    Run tf0.12 upgrade on dynamodb module

--- example/dynamodb.tf
+++ example/dynamodb.tf
@@ -25,10 +25,11 @@ resource "kubernetes_secret" "example_team_dynamodb" {
     namespace = "my-namespace"
   }
 
-  data {
-    table_name        = "${module.example_team_dynamodb.table_name}"
-    table_arn         = "${module.example_team_dynamodb.table_arn}"
-    access_key_id     = "${module.example_team_dynamodb.access_key_id}"
-    secret_access_key = "${module.example_team_dynamodb.secret_access_key}"
+  data = {
+    table_name        = module.example_team_dynamodb.table_name
+    table_arn         = module.example_team_dynamodb.table_arn
+    access_key_id     = module.example_team_dynamodb.access_key_id
+    secret_access_key = module.example_team_dynamodb.secret_access_key
   }
 }
+
--- example/main.tf
+++ example/main.tf
@@ -1,7 +1,10 @@
-# terraform {
-#   backend "s3" {}
-# }
+terraform {
+  backend "s3" {
+
+  }
+}
 
 provider "aws" {
   region = "eu-west-2"
 }
+
