b4ce33fcc46844ee5a2bc98843bad47ddac5ddd1
jakemulley <jakemulley@users.noreply.github.com>
2022-12-28 14:33:08 +0000

    Update reference to environment-name

--- example/dynamodb.tf
+++ example/dynamodb.tf
@@ -11,7 +11,7 @@ module "example_team_dynamodb" {
   business-unit          = var.business_unit
   application            = var.application
   is-production          = var.is_production
-  environment-name       = var.environment
+  environment-name       = var.environment_name
   infrastructure-support = var.infrastructure_support
   aws_region             = "eu-west-2"
   namespace              = var.namespace
