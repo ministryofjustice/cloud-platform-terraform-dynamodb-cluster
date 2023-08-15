b2e634b00e118c3089f3fb414a974d0b4fbf8ec4
jakemulley <jakemulley@users.noreply.github.com>
2023-08-15 14:05:31 +0100

    Fix example

--- examples/dynamodb.tf
+++ examples/dynamodb.tf
@@ -8,11 +8,11 @@ module "example_team_dynamodb" {
   source = "../"
 
   team_name              = var.team_name
-  business-unit          = var.business_unit
+  business_unit          = var.business_unit
   application            = var.application
-  is-production          = var.is_production
-  environment-name       = var.environment_name
-  infrastructure-support = var.infrastructure_support
+  is_production          = var.is_production
+  environment_name       = var.environment_name
+  infrastructure_support = var.infrastructure_support
   namespace              = var.namespace
 
   hash_key  = "example-hash"
@@ -46,4 +46,3 @@ resource "kubernetes_secret" "example_team_dynamodb" {
     secret_access_key = module.example_team_dynamodb.secret_access_key
   }
 }
-
